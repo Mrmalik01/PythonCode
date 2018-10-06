e5b490815db45302d9927a78824ea84abf850160
Khizar Malik <k.malik@se17.qmul.ac.uk>
2018-10-06 13:19:51 +0100

    Without extra whitespace

--- insertionSort.py
+++ insertionSort.py
@@ -1,22 +1,13 @@
 def insertionSort(A):
-     # compare A[i] with all A[0]...A[i] elements
-     for i in range(1,len(A)):
-          insert(A[i],i,A)
-          
-def insert(k,hi,A):
-     # compare k with all A[0]...A[i]
-     for i in range(hi,0,-1):
-          # if k is greater or equals to A[i-1], then assign A[i] to k
-          if k >= A[i-1]:
-               # if k is greater than the last element then insert k in empty or copied position (A[i])
-               A[i] = k
+     for i in range(1,len(A)): # compare A[i] with all A[0]...A[i] elements
+          insert(A[i],i,A) 
+def insert(k,hi,A):    
+     for i in range(hi,0,-1): # compare k with all A[0]...A[i]
+          if k >= A[i-1]: # if k is greater or equals to A[i-1], then assign A[i] to k
+               A[i] = k # if k is greater than the last element then insert k in empty or copied position (A[i])
                return
-          # else move A[i-1] element to one index ahead, such that A[i-1] is empty or copy of A[i]
-          A[i]= A[i-1]
+          A[i]= A[i-1] # else move A[i-1] element to one index ahead, such that A[i-1] is empty or copy of A[i]
      A[0] = k
-
-
-
 array = [4,3,5,0,1,6,7,9,10]
 insertionSort(array)
 print(array)
--- selectionSort.py
+++ selectionSort.py
@@ -1,19 +1,14 @@
 def selectionSort(A):
-     #compare A[i] element with Remaining elements 
-     for i in range(len(A)):
+     for i in range(len(A)): #compare A[i] element with Remaining elements
           smallest = findSmall(A,i) # Find the smallest one from ith element
           swap(i,smallest,A) # replace with ith element
-
-# find the smallest number from ith element
-def findSmall(A,i):
+def findSmall(A,i): # find the smallest number from ith element
      small = i
      for f in range(i+1,len(A)):
           if A[small] > A[f]:
                small = f
      return small
-
-# Swap the A[ini] with A[small] element
-def swap(ini,small,A):
+def swap(ini,small,A): # Swap the A[ini] with A[small] element
      helper = A[ini]
      A[ini] = A[small]
      A[small] = helper
