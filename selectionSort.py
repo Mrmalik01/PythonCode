f251edf7ca2481ef3db8099394ced754227a5193
Khizar Malik <k.malik@se17.qmul.ac.uk>
2018-10-06 12:39:30 +0100

    Selection Sort comment added

--- selectionSort.py
+++ selectionSort.py
@@ -1,9 +1,10 @@
 def selectionSort(A):
+     #compare A[i] element with Remaining elements 
      for i in range(len(A)):
-          smallest = findSmall(A,i)
-          swap(i,smallest,A)
+          smallest = findSmall(A,i) # Find the smallest one from ith element
+          swap(i,smallest,A) # replace with ith element
 
-
+# find the smallest number from ith element
 def findSmall(A,i):
      small = i
      for f in range(i+1,len(A)):
@@ -11,6 +12,7 @@ def findSmall(A,i):
                small = f
      return small
 
+# Swap the A[ini] with A[small] element
 def swap(ini,small,A):
      helper = A[ini]
      A[ini] = A[small]
